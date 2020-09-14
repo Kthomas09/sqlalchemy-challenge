c8bfec4d10889d4874e0cb37aa58f55a70e659df
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-09-13 21:29:44 -0400

    Worked on the climate app.py using flask. There is a bug in the app that has to do with my virtual environment. Must get the answer to this question before proceeding.

---
+++ climateapp.py
@@ -0,0 +1,72 @@
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+import numpy as np
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+app = Flask(__name__)
+
+session = Session(engine)
+
+@app.route("/")
+def welcome():
+    """List of API Routes."""
+    
+    return """<html><a href = "/api/v1.0/precipitation">/api/v1.0/precipitation</a>,
+            <a href = "/api/v1.0/<start>">/api/v1.0/<start></a>,
+            <a href = "/api/v1.0/<start>/<end>">/api/v1.0/<start>/<end></a>
+            <a href = /api/v1.0/station">/api/v1.0/station</a></html>"""
+
+session.close
+
+@app.route("/api/v1.0/precipitation")
+def precipitiation():
+    session = Session(engine)
+    max_date = dt.date(2012, 2, 28)
+    last_year = max_date - dt.timedelta(days=365)
+    
+    past_temp = (session.query(measurement.date, measurement.prcp).filter(measurement.date <= max_date).filter(measurement.date >= last_year).order_by(measurement.date).all())
+    
+    rainfall = {date: prcp for date, prcp in past_temp}
+    
+    return jsonify(rainfall)
+
+session.close()
+@app.route("/api/v1.0/<start>")
+def start (start = None):
+    session = Session(engine)
+    tobs_only = (session.query(measurement.tobs).filter(measurement.date.between(start, "2012-03-05")).all())
+    
+    return jsonify(tobs_only)
+
+session.close()
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start = None, end = None):
+    session = Session(engine)
+    tobs_only = (session.query(measurement.tobs).filter(measurement.date.between(start, end)).all())
+    
+    return jsonify(tobs_only)
+
+session.close()
+
+@app.route("/api/v1.0/station")
+def station():
+    stations = session.query(station.name, station.station).all()
+    stationls = list(np.ravel(stations))
+    return jsonify(stationls)
+
+session.close()
+
+if __name__ == "__main__":
+    app.run(debug=True)
